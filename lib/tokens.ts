01a3c56af745bd3c694795498c4d7bf9b4a907a5
Aditya Bhattad <adityabhattad18@gmail.com>
2024-01-31 16:13:26 +0530

    feat: add generate two factor auth function

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -3,6 +3,32 @@ import { v4 as uuidv4 } from "uuid";
 import crypto from "crypto";
 import { db } from "./db";
 import { getPasswordResetTokenByEmail } from "@/utils/password-reset-token";
+import { getTwoFactorAuthTokenByEmail } from "@/utils/two-factor-auth-token";
+import { TwoFactorToken } from "@prisma/client";
+
+export async function generateTwoFactorAuthToken(email:string):Promise<TwoFactorToken>{
+    const token = crypto.randomInt(100_000,1_000_000).toString();
+    const expires = new Date(new Date().getTime()+10*60*1000);
+
+    const existingToken = await getTwoFactorAuthTokenByEmail(email);
+    if(existingToken){
+        await db.twoFactorToken.delete({
+            where:{
+                id:existingToken.id
+            }
+        })
+    }
+
+    const newTwoFactorAuthToken = await db.twoFactorToken.create({
+        data:{
+            email,
+            token,
+            expires
+        }
+    })
+
+    return newTwoFactorAuthToken;
+}
 
 export async function generateVerificationToken(email: string) {
     const token = uuidv4();
