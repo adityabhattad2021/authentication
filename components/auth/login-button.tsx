ea04771439912e5f82438f7e3c0147fe8d8c63cd
Aditya Bhattad <adityabhattad18@gmail.com>
2024-02-04 14:44:51 +0530

    perf: add login form in modal instead of redirect

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -1,32 +1,39 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
+import { LoginForm } from "./login-form";
 
 interface LoginButtonProps {
-    children:React.ReactNode;
-    mode:"modal" | "redirect";
-    asChild?:boolean;
+    children: React.ReactNode;
+    mode: "modal" | "redirect";
+    asChild?: boolean;
 }
 
 export default function LoginButton({
     children,
-    mode="redirect",
+    mode = "redirect",
     asChild
-}:LoginButtonProps){
+}: LoginButtonProps) {
 
     const router = useRouter();
 
-    function onClick(){
+    function onClick() {
         router.push('/auth/login');
     }
 
-    if(mode==="modal"){
+    if (mode === "modal") {
         return (
-            <div>
-                TODO: Add Modal
-            </div>
+            <Dialog>
+                <DialogTrigger asChild={asChild}>
+                    {children}
+                </DialogTrigger>
+                <DialogContent className="p-0 w-auto bg-transparent border-none">
+                    <LoginForm />
+                </DialogContent>
+            </Dialog>
         )
     }
-    
+
     return (
         <span onClick={onClick} className="cursor-pointer">
             {children}
